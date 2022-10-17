853240956c7d795ff8a9d235c19235918bf0bdd2
devcom2020 <kwp@tlen.pl>
2022-10-17 15:36:38 +0200

    poniedziałek 15:36

---
+++ orange3/bank.py
@@ -0,0 +1,26 @@
+# Ten program ustala, czy klient banku
+# ma zdolność kredytową.
+
+MIN_SALARY = 30000.0  # Wysokość minimalnego rocznego wynagrodzenia.
+MIN_YEARS = 2  # Minimalna liczba lat pracy u aktualnego pracodawcy.
+
+# Pobranie wynagrodzenia klienta.
+salary = float(input('Podaj wysokość rocznego wynagrodzenia: '))
+
+# Pobranie liczby lat pracy u aktualnego pracodawcy.
+years_on_job = int(input('Podaj liczbę lat pracy ' +
+                         'u obecnego pracodawcy: '))
+
+# Ustalenie, czy klient ma zdolność kredytową.
+# if salary >= MIN_SALARY and years_on_job >= MIN_YEARS:
+#     print('Masz zdolność kredytową.')
+# else:
+#     print('Nie masz zdolności kredytowej.')
+
+if salary >= MIN_SALARY:
+    if years_on_job >= MIN_YEARS:
+        print('Masz zdolność kredytową.')
+    else:
+        print('Musisz być zatrudniony', MIN_YEARS)
+else:
+    print('Musisz zarabiać minimum:', MIN_SALARY)
