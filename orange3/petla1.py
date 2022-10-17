b98fd7de53ee4418f49f29ef5081ab1c19e56921
devcom2020 <kwp@tlen.pl>
2022-10-17 16:04:45 +0200

    poniedziałek 16:04

---
+++ orange3/petla1.py
@@ -0,0 +1,24 @@
+# keep_going = 't'
+#
+# while keep_going == 't':
+#     orig_price = float(input("Podaj cenę detaliczną:"))
+#     rabat = orig_price * 0.2
+#     cena_sprz = orig_price - rabat
+#     print("cena finalna=", cena_sprz)
+#     print("-" * 20)
+#     keep_going = input("Czy chcesz obliczyć kolejną cenę?(Jeśli tak, wpisz 't'):")
+
+licznik = 0
+while True:
+    licznik += 1  # licznik = licznik + 1
+    if licznik > 10:
+        print(licznik)
+        break
+
+napis = "15"
+for litera in napis:
+    print(litera)
+
+for i in range(10):
+    print("hej, jestem w iteracji nr", i)
+    print(i + 3)
