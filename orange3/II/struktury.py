b34bb563fb5eebe508387cae0de2233a2fb67392
devcom2020 <kwp@tlen.pl>
2022-10-18 09:27:35 +0200

    wtorek 09:27

--- orange3/II/struktury.py
+++ orange3/II/struktury.py
@@ -1,5 +1,17 @@
-for hours in range(24):
-    for minutes in range(60):
-        for seconds in range(60):
-            print(hours, minutes, seconds, sep=":")
+# for hours in range(24):
+#     for minutes in range(60):
+#         for seconds in range(60):
+#             print(hours, minutes, seconds, sep=":")
 
+rows = 5
+cols = 10
+
+# for r in range(rows):
+#     for c in range(cols):
+#         print('*', end='^')
+#     print()
+
+for r in range(rows):
+    for c in range(r + 1):
+        print('*', end='')
+    print("o")
