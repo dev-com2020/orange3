e982ff42329054bef9982e723c664a280ea63a29
devcom2020 <kwp@tlen.pl>
2022-10-19 12:31:24 +0200

    sroda 12:31

---
+++ orange3/III/baza.py
@@ -0,0 +1,39 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+# create_connection('sqlite-sakila.db')
+
+database = "baza_nr1.sqlite"
+
+sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+
+conn = create_connection(database)
+
+
--- orange3/III/xmlki.py
+++ orange3/III/xmlki.py
@@ -1,13 +1,41 @@
-from xml.dom import minidom
+# import json
+#
+# import xmltodict
+#
+# with open("hello.svg") as xml_file:
+#     data_dict = xmltodict.parse(xml_file.read())
+#
+# json_data = json.dumps(data_dict, indent=4)
+#
+# with open("nowe_dane.json", 'w') as json_file:
+#     json_file.write(json_data)
 
-doc = minidom.parse('test.xml')
 
-print(doc.toxml())
-nodes = doc.childNodes
-for i in nodes[0].getElementsByTagName("osoba"):
-    print(i.getElementsByTagName("imie")[0].nodeName)
-    print(i.getElementsByTagName("imie")[0].childNodes[0].toxml())
-    print(i.getElementsByTagName("imie")[0].getAttribute("foo"))
+import xml.etree.ElementTree as E
+
+tree = E.parse('test.xml')
+root = tree.getroot()
+d = {}
+for child in root:
+    if child.tag not in d:
+        d[child.tag] = []
+    dic = {}
+    for child2 in child:
+        if child2.tag not in dic:
+            dic[child2.tag] = child2.text
+    d[child.tag].append(dic)
+print(d)
+
+# from xml.dom import minidom
+#
+# doc = minidom.parse('test.xml')
+#
+# print(doc.toxml())
+# nodes = doc.childNodes
+# for i in nodes[0].getElementsByTagName("osoba"):
+#     print(i.getElementsByTagName("imie")[0].nodeName)
+#     print(i.getElementsByTagName("imie")[0].childNodes[0].toxml())
+#     print(i.getElementsByTagName("imie")[0].getAttribute("foo"))
 
 
 # with open("hello.svg") as file:
@@ -19,11 +47,8 @@ for i in nodes[0].getElementsByTagName("osoba"):
 #             print(child)
 
 
-
 # print(root)
 
 # element = doc.getElementById("smiley")
 # print(element.parentNode)
 # print(element.firstChild)
-
-
