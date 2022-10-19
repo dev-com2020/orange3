c403add1950138820a9fd31d1e24d05845b6d50e
devcom2020 <kwp@tlen.pl>
2022-10-19 12:58:55 +0200

    sroda 12:58

--- orange3/III/baza.py
+++ orange3/III/baza.py
@@ -1,17 +1,53 @@
 import sqlite3
 from sqlite3 import Error
 
+
 def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    return conn
 
+
+def create_table(conn, create_table_sql):
+    """ tworzymy tabelę w bazie
+    :param conn: Connection object
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def create_project(conn, project):
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+
+def update_project(conn, project):
+    sql = ''' UPDATE projects
+              SET name = ? ,
+                  begin_date = ?,
+                  end_date = ?
+              WHERE id = ? '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+def select_all_projects(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM projects")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 # create_connection('sqlite-sakila.db')
 
@@ -36,4 +72,10 @@ sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
 
 conn = create_connection(database)
 
-
+if conn is not None:
+    # create_table(conn, sql_create_projects_table)
+    # create_table(conn, sql_create_tasks_table)
+    # project = ("Szkolenie Comarch Java", '2023-10-17', '2023-10-19')
+    # project_id = create_project(conn, project)
+    # update_project(conn, ('wakacje', '2023-07-17', '2023-08-19', 2))
+    select_all_projects(conn)
